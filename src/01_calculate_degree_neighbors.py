b7ce440e8ba92e14c962cb8723b1458533aa6418
Daniel Chen <Maveri2k@yahoo.com>
2016-05-08 23:50:44 -0400

    use nearest 1 and 3 instead of 5 for disk space

--- src/01_calculate_degree_neighbors.py
+++ src/01_calculate_degree_neighbors.py
@@ -70,9 +70,9 @@ def main():
     nx.set_node_attributes(g, 'degree', degree_attributes)
     # nx.set_node_attributes(g, 'katz', katz_attributes)
 
-    # nearest_1_attributes = {}  # c
-    # nearest_3_attributes = {}  # c
-    nearest_5_attributes = {}
+    nearest_1_attributes = {}  # c
+    nearest_3_attributes = {}  # c
+    # nearest_5_attributes = {}
     max_degree_head_tail_attributes = {}  # c
     min_degree_head_tail_attributes = {}  # c
     avg_degree_head_tail_attributes = {}  # c
@@ -82,14 +82,14 @@ def main():
     for node in tqdm.tqdm(g.nodes_iter()):
         match = edge_node_pattern.match(node)
         if match:
-            # n1 = cna.calculate_nearest_k_nodes(g, node, 2)
-            # nearest_2_attributes[node] = n2
+            n1 = cna.calculate_nearest_k_nodes(g, node, 2)
+            nearest_1_attributes[node] = n1
 
-            # n3 = cna.calculate_nearest_k_nodes(g, node, 4)
-            # nearest_4_attributes[node] = n4
+            n3 = cna.calculate_nearest_k_nodes(g, node, 4)
+            nearest_3_attributes[node] = n3
 
-            n5 = cna.calculate_nearest_k_nodes(g, node, 5)
-            nearest_5_attributes[node] = n5
+            # n5 = cna.calculate_nearest_k_nodes(g, node, 5)
+            # nearest_5_attributes[node] = n5
 
             max_d_ht = cna.calculate_max_degree_head_tail(g, node)
             max_degree_head_tail_attributes[node] = max_d_ht
@@ -101,9 +101,9 @@ def main():
             avg_degree_head_tail_attributes[node] = avg_d_ht
 
     print('setting node attributes')
-    # nx.set_node_attributes(g, 'nearest_1',  nearest_1_attributes)
-    # nx.set_node_attributes(g, 'nearest_3',  nearest_3_attributes)
-    nx.set_node_attributes(g, 'nearest_5',  nearest_5_attributes)
+    nx.set_node_attributes(g, 'nearest_1',  nearest_1_attributes)
+    nx.set_node_attributes(g, 'nearest_3',  nearest_3_attributes)
+    # nx.set_node_attributes(g, 'nearest_5',  nearest_5_attributes)
     nx.set_node_attributes(g, 'max_degree_head_tail',
                            max_degree_head_tail_attributes)
     nx.set_node_attributes(g, 'min_degree_head_tail',
@@ -116,9 +116,9 @@ def main():
                      # 'betweenness',
                      'degree',
                      # 'katz',
-                     # 'nearest_2',
-                     # 'nearest_4',
-                     'nearest_5',
+                     'nearest_1',
+                     'nearest_3',
+                     # 'nearest_5',
                      'max_degree_head_tail', 'min_degree_head_tail',
                      'avg_degree_head_tail']
         fstring = ['%s'] * len(col_names)
@@ -137,9 +137,9 @@ def main():
                     # na['betweenness'],
                     na['degree'],
                     # na['katz'],
-                    # na['nearest_1'],
-                    # na['nearest_3'],
-                    na['nearest_5'],
+                    na['nearest_1'],
+                    na['nearest_3'],
+                    # na['nearest_5'],
                     na['max_degree_head_tail'], na['min_degree_head_tail'],
                     na['avg_degree_head_tail']
                 ]))
