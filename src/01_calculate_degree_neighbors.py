f5e001dec8ca49529b2a39c6eb5b86f090dd3609
Daniel Chen <Maveri2k@yahoo.com>
2016-05-08 22:10:37 -0400

    interactome, edgenode only, calculate neighbor 5

--- src/01_calculate_degree_neighbors.py
+++ src/01_calculate_degree_neighbors.py
@@ -1,5 +1,9 @@
+import re
+
 import networkx as nx
 import pandas as pd
+
+import tqdm
 
 import calculate_node_attributes as cna
 import helper
@@ -30,41 +34,66 @@ def setup():
 def main():
     pathways, interactome = setup()
 
-    for pathway_name in pathways:
-        pathway_df = pd.read_csv('../data/pathways/{}-edges.txt'.
-                                 format(pathway_name),
-                                 delimiter='\t')
+    # I fucked up.
+    # this file should not loop over any of the pathways
+    # the features here should be calculated on the entire interactome
+    # this is why the variable names will be confusing
 
-        pathway_df_n2e = helper.convert_edges_to_node(pathway_df)
+    # for pathway_name in pathways:
+    #     pathway_df = pd.read_csv('../data/pathways/{}-edges.txt'.
+    #                          format(pathway_name),
+    #                          delimiter='\t')
 
-        g = nx.from_pandas_dataframe(
-            pathway_df_n2e, source='#tail', target='head',
-            create_using=nx.DiGraph())
+    pathway_name = 'interactome'
+    pathway_df = interactome
 
-        betweenness_attributes = cna.calculate_betweenesss(g)
-        degree_attributes = cna.calculate_degree(g)
-        katz_attributes = cna.calculate_katz(g)
+    pathway_df_n2e = helper.convert_edges_to_node(
+        pathway_df, weight_col='edge_weight')
 
-        nx.set_node_attributes(g, 'betweenness', betweenness_attributes)
-        nx.set_node_attributes(g, 'degree', degree_attributes)
-        nx.set_node_attributes(g, 'katz', katz_attributes)
+    g = nx.from_pandas_dataframe(
+        pathway_df_n2e, source='#tail', target='head',
+        create_using=nx.DiGraph())
 
-        nearest_2_attributes = {}  # c
-        nearest_4_attributes = {}  # c
-        nearest_6_attributes = {}  # c
-        max_degree_head_tail_attributes = {}  # c
-        min_degree_head_tail_attributes = {}  # c
-        avg_degree_head_tail_attributes = {}  # c
+    # print('calculating betweenness_attributes')
+    # betweenness_attributes = cna.calculate_betweenesss(g)
+    # print('finished betweenness_attributes')
 
-        for node in g.nodes_iter():
-            n2 = cna.calculate_nearest_k_nodes(g, node, 2)
-            nearest_2_attributes[node] = n2
+    print('calculating degree_attributes')
+    degree_attributes = cna.calculate_degree(g)
+    print('finished degree_attributes')
 
-            n4 = cna.calculate_nearest_k_nodes(g, node, 4)
-            nearest_4_attributes[node] = n4
+    # print('calculating katz_attributes')
+    # katz_attributes = cna.calculate_katz(g)
+    # print('finished katz_attributes')
 
-            n6 = cna.calculate_nearest_k_nodes(g, node, 6)
-            nearest_6_attributes[node] = n6
+    # nx.set_node_attributes(g, 'betweenness', betweenness_attributes)
+    nx.set_node_attributes(g, 'degree', degree_attributes)
+    # nx.set_node_attributes(g, 'katz', katz_attributes)
+
+    # nearest_2_attributes = {}  # c
+    # nearest_4_attributes = {}  # c
+    # nearest_6_attributes = {}  # c
+    nearest_5_attributes = {}
+    max_degree_head_tail_attributes = {}  # c
+    min_degree_head_tail_attributes = {}  # c
+    avg_degree_head_tail_attributes = {}  # c
+
+    edge_node_pattern = re.compile('.*_to_.*')
+    print('iterating over nodes')
+    for node in tqdm.tqdm(g.nodes_iter()):
+        match = edge_node_pattern.match(node)
+        if match:
+            # n2 = cna.calculate_nearest_k_nodes(g, node, 2)
+            # nearest_2_attributes[node] = n2
+
+            # n4 = cna.calculate_nearest_k_nodes(g, node, 4)
+            # nearest_4_attributes[node] = n4
+
+            # n6 = cna.calculate_nearest_k_nodes(g, node, 6)
+            # nearest_6_attributes[node] = n6
+
+            n5 = cna.calculate_nearest_k_nodes(g, node, 5)
+            nearest_5_attributes[node] = n5
 
             max_d_ht = cna.calculate_max_degree_head_tail(g, node)
             max_degree_head_tail_attributes[node] = max_d_ht
@@ -75,36 +104,44 @@ def main():
             avg_d_ht = cna.calculate_avg_degree_head_tail(g, node)
             avg_degree_head_tail_attributes[node] = avg_d_ht
 
-        nx.set_node_attributes(g, 'nearest_2',  nearest_2_attributes)
-        nx.set_node_attributes(g, 'nearest_4',  nearest_2_attributes)
-        nx.set_node_attributes(g, 'nearest_6',  nearest_2_attributes)
-        nx.set_node_attributes(g, 'max_degree_head_tail',
-                               max_degree_head_tail_attributes)
-        nx.set_node_attributes(g, 'min_degree_head_tail',
-                               min_degree_head_tail_attributes)
-        nx.set_node_attributes(g, 'avg_degree_head_tail',
-                               avg_degree_head_tail_attributes)
-        node_filename = '../output/features_{}_01.txt'.format(pathway_name)
-        with open(node_filename, 'w') as f:
-            col_names = ['name', 'betweenness', 'degree', 'katz',
-                         'nearest_2', 'nearest_4', 'nearest_6',
-                         'max_degree_head_tail', 'min_degree_head_tail',
-                         'avg_degree_head_tail']
-            fstring = ['%s'] * len(col_names)
-            fstring = '\t'.join(fstring) + '\n'
+    print('setting node attributes')
+    # nx.set_node_attributes(g, 'nearest_2',  nearest_2_attributes)
+    # nx.set_node_attributes(g, 'nearest_4',  nearest_2_attributes)
+    # nx.set_node_attributes(g, 'nearest_6',  nearest_2_attributes)
+    nx.set_node_attributes(g, 'max_degree_head_tail',
+                           max_degree_head_tail_attributes)
+    nx.set_node_attributes(g, 'min_degree_head_tail',
+                           min_degree_head_tail_attributes)
+    nx.set_node_attributes(g, 'avg_degree_head_tail',
+                           avg_degree_head_tail_attributes)
+    node_filename = '../output/features_{}_01.txt'.format(pathway_name)
+    with open(node_filename, 'w') as f:
+        col_names = ['name',  # 'betweenness',
+                     'degree',
+                     # 'katz',
+                     'nearest_2', 'nearest_4', 'nearest_6',
+                     'max_degree_head_tail', 'min_degree_head_tail',
+                     'avg_degree_head_tail']
+        fstring = ['%s'] * len(col_names)
+        fstring = '\t'.join(fstring) + '\n'
 
-            wstring = fstring % tuple(col_names)
-            f.write(wstring)
+        wstring = fstring % tuple(col_names)
+        f.write(wstring)
 
-            for node in g.nodes_iter():
+        print('writing node info')
+        for node in tqdm.tqdm(g.nodes_iter()):
+            match = edge_node_pattern.match(node)
+            if match:
                 na = g.node[node]
                 f.write(fstring % tuple([
-                    node, na['betweenness'],  na['degree'], na['katz'],
+                    node,  # na['betweenness'],
+                    na['degree'],
+                    # na['katz'],
                     na['nearest_2'], na['nearest_4'], na['nearest_6'],
                     na['max_degree_head_tail'], na['min_degree_head_tail'],
                     na['avg_degree_head_tail']
                 ]))
-        print("{} created".format(node_filename))
+    print("{} created".format(node_filename))
 
 if __name__ == '__main__':
     main()
